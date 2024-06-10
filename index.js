d51d94101050136a336fef950170cccd5c38ec2d
Lovisa Viigisalu <l.viigisalu@gmail.com>
2024-06-10 18:32:30 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) =>{
-    res.json({message: "Welcome to sequelize application."})
+const Sequelize = require("sequelize");
+// Update the connection string to include the password
+const sequelize = new Sequelize("mysql://user:user@localhost:3306/joga_sequelize");
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database.');
+    })
+    .catch(err => {
+        console.error("Unable to connect: ", err);
+    });
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to sequelize application" });
 });
+
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-})
+    console.log('Server is running on http://localhost:3000');
+});
