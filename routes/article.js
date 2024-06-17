3df149a7a92fd15476dbb4f31b353cce5f336186
Lovisa Viigisalu <l.viigisalu@gmail.com>
2024-06-17 03:50:58 +0300

    Created new article

--- routes/article.js
+++ routes/article.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ArticleController = require('../Controllers/article')
 const authorController = require('../Controllers/article')
+const articleAdminController = require('../Controllers/admin/articles')
 
 router.get('/', ArticleController.getAllArticles);
 router.get('/article/:slug', ArticleController.getArticleBySlug);
 router.get('/author/:id', authorController.getArticlesByAuthor);
-
+router.post('/admin/article/create', articleAdminController.createArticle)
 
 module.exports = router;
